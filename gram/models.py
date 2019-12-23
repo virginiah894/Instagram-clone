17326e022d25dcef7e6a39c9c5d9402fc2936e16
virginiah894 <virgyperry@gmail.com>
2019-12-23 08:59:18 +0300

    Add a post model

--- gram/models.py
+++ gram/models.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model)
+poster=models.ForeignKey('auth-user',on_delete=models.CASCADE)
+image = models.ImageField(null=True)
+caption = models.TextField()
+posted_date = models.DateTimeField(default=timezone.now
