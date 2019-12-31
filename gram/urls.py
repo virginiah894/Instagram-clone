8f96dcafa25dc4a9ab73ebafbc4ed953ec35659d
virginiah894 <virgyperry@gmail.com>
2019-12-31 23:07:23 +0300

    add path for the images

--- gram/urls.py
+++ gram/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from .views import (
   PostCreateView,
   PostListView,
@@ -8,3 +10,5 @@ urlpatterns = [
   path('',PostListView.as_view(),name='allPosts'),
     path('new/',PostCreateView.as_view(),name='createPost'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
