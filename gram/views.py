0cb08be861de6ce5b745b84b07e8d9e4550e8318
virginiah894 <virgyperry@gmail.com>
2020-01-01 17:48:50 +0300

    update route for searching

--- gram/views.py
+++ gram/views.py
@@ -5,10 +5,12 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 from .forms import PostForm
+from django.http import HttpResponse
 from django.views.generic import(
   ListView,
   CreateView,
   DetailView,
+
 )
 
 # Create your views here.
@@ -34,3 +36,13 @@ class PostDetailView(DetailView):
     id_=self.kwargs.get('id')
     return get_object_or_404(Post,id=id_)
     template_name = 'gram/post_detail.html'
+# searching view
+def search_posts(request):
+    if 'user' in request.GET and request.GET['user']:
+      search_term = request.GET.get('user')
+      searched_users = Post.search_by_author(search_term)
+      message = f'{search_term}'
+      return render (request, 'gram/search.html',{'message':message,'users':searched_users})
+    else:
+      message = "Nothing was searched.Please ensure you type something"
+      return render (request, 'gram/search.html',{'message':message})
