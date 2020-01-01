8dc48de10cc9b85f97f23f306e9078d4c52efd19
virginiah894 <virgyperry@gmail.com>
2020-01-01 16:00:23 +0300

    create templates to update posts  and get  post detials

--- gram/urls.py
+++ gram/urls.py
@@ -4,11 +4,13 @@ from django.conf import settings
 from .views import (
   PostCreateView,
   PostListView,
+  PostDetailView,
 )
 app_name = 'gram'
 urlpatterns = [
-  path('',PostListView.as_view(),name='allPosts'),
+    path('',PostListView.as_view(),name='allPosts'),
     path('new/',PostCreateView.as_view(),name='createPost'),
+    path('<int:id>',PostDetailView.as_view(), name= 'postDetails'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
--- gram/views.py
+++ gram/views.py
@@ -1,28 +1,36 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.utils import timezone
 from .forms import PostForm
 from django.views.generic import(
   ListView,
   CreateView,
+  DetailView,
 )
 
 # Create your views here.
 class PostListView(ListView):
-  template_name = 'gramm/all posts.html'
-  queryset = Post.objects.all()
+  template_name = 'gram/all posts.html'
+  queryset = Post.objects.all().filter(posted_date__lte=timezone.now()).order_by('-posted_date')
   context_object_name ='posts'
 
 class PostCreateView(CreateView):
-  template_name = 'gramm/create.html'
+  template_name = 'gram/create.html'
   form_class = PostForm
   queryset = Post.objects.all()
   success_url = '/' 
 
-  def form_valid(self):
+  def form_valid(self,form):
      print (form.cleaned_data)
      form.instance.author = self.request.user
      return super().form_valid(form)
 
+class PostDetailView(DetailView):
+  queryset=Post.objects.all().filter(posted_date__lte=timezone.now())
+  def get_object(self):
+    id_=self.kwargs.get('id')
+    return get_object_or_404(Post,id=id_)
+    template_name = 'gram/post_detail.html'
