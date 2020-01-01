7a03f49b9bf53f3ca3f3911c565bb0fc8912a4cb
virginiah894 <virgyperry@gmail.com>
2020-01-01 17:47:33 +0300

    add path for searching users

--- gram/urls.py
+++ gram/urls.py
@@ -1,16 +1,19 @@
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from . import views
 from .views import (
   PostCreateView,
   PostListView,
   PostDetailView,
+
 )
 app_name = 'gram'
 urlpatterns = [
     path('',PostListView.as_view(),name='allPosts'),
     path('new/',PostCreateView.as_view(),name='createPost'),
     path('<int:id>',PostDetailView.as_view(), name= 'postDetails'),
+    path('search/',views.search_posts ,name='search_posts'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
